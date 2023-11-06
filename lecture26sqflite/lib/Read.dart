e2c659571daad1564731c5352643050ce26b461c
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2023-11-06 20:52:06 +0500

    lecture update & delete

--- lecture26sqflite/lib/Read.dart
+++ lecture26sqflite/lib/Read.dart
@@ -11,7 +11,7 @@ class Read extends StatefulWidget {
 
 class _ReadState extends State<Read> {
   List<Map<String, dynamic>> datalist = [];
-
+  TextEditingController nameController = TextEditingController();
   void alldata() async {
     var query = await Db_helper.instance.queryDatabase();
     setState(() {
@@ -34,23 +34,66 @@ class _ReadState extends State<Read> {
         body: ListView.builder(
       itemBuilder: (context, index) {
         return ListTile(
-                title: Text(datalist[index]["name"].toString()),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
-                    IconButton(
-                        onPressed: () {
-                          Db_helper.instance
-                              .deleteRecord(datalist[index]["id"]);
-                          alldata();
-                        },
-                        icon: Icon(Icons.delete))
-                  ],
-                ),
-              );
+          title: Text(datalist[index]["name"].toString()),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              IconButton(
+                  onPressed: () {
+                    var id = datalist[index]["id"];
+
+                    var name = datalist[index]["name"].toString();
+
+                    update(id, name);
+                  },
+                  icon: Icon(Icons.edit)),
+              IconButton(
+                  onPressed: () {
+                    Db_helper.instance.deleteRecord(datalist[index]["id"]);
+                    alldata();
+                  },
+                  icon: Icon(Icons.delete))
+            ],
+          ),
+        );
       },
       itemCount: datalist.length,
     ));
+  }
+
+  update(id, name) {
+    nameController.text = name;
+
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (context) {
+        return Padding(
+          padding: EdgeInsets.fromLTRB(
+              32, 32, 32, MediaQuery.of(context).viewInsets.bottom),
+          child: Column(
+          mainAxisSize: MainAxisSize.min,
+            children: [
+              Column(mainAxisSize: MainAxisSize.min,
+               children: [
+                TextField(
+                  controller: nameController,
+                ),
+                ElevatedButton(
+                    onPressed: () async {
+                      var name = nameController.text.toString();
+
+                      await Db_helper.instance.updateRecord(
+                          {Db_helper.dt_id: id, Db_helper.dt_name: name});
+                      alldata();
+                      Navigator.of(context).pop();
+                    },
+                    child: Text("Update"))
+              ]),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
